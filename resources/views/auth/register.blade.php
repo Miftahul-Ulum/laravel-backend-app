7b056792aacc188b624ad8c4fdd4ac60bdcf8d46
Miftahul-Ulum <aqilajati7@gmail.com>
2023-10-27 21:16:10 +0700

    Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,23 +16,38 @@
 
         <div class="card-body">
             <form method="POST">
+                @csrf
                     <div class="form-group">
                         <label for="frist_name">Name</label>
                         <input id="frist_name"
                             type="text"
-                            class="form-control"
+                            class="form-control @error('name')
+                                is-invalid
+                            @enderror"
                             name="name"
                             autofocus>
+                            @error('name')
+                        <div class="invalid-feedback">
+                            {{$message }}
+                        </div>  
+                        @enderror
                     </div>
 
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input id="email"
                         type="email"
-                        class="form-control"
+                        class="form-control @error('email')
+                            is-invalid
+                        @enderror"
                         name="email">
                     <div class="invalid-feedback">
                     </div>
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{$message }}
+                        </div>  
+                        @enderror
                 </div>
 
                     <div class="form-group">
@@ -40,8 +55,15 @@
                             class="d-block">Password</label>
                         <input id="password"
                             type="password"
-                            class="form-control"
+                            class="form-control @error('password')
+                                is-invalid
+                            @enderror"
                             name="password">
+                            @error('password')
+                        <div class="invalid-feedback">
+                            {{$message }}
+                        </div>  
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label for="password2"
