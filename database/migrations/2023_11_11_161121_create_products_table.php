14e57d0c3ecdc88d02191f2e48095bf97f59bfe7
Miftahul-Ulum <aqilajati7@gmail.com>
2023-11-11 23:23:12 +0700

    edit controller

--- database/migrations/2023_11_11_161121_create_products_table.php
+++ database/migrations/2023_11_11_161121_create_products_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Category;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->double('price');
+            $table->string('image_url');
+            $table->foreignIdFor(Category::class);
+            $table->foreignIdFor(User::class);
             $table->timestamps();
         });
     }
