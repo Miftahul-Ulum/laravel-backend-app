0d8cb00a453b7a0b481ead31de780e6f8652e909
Miftahul-Ulum <aqilajati7@gmail.com>
2023-12-11 22:12:05 +0700

    Membuat Kategory dan Product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,27 +2,24 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
-use App\Models\User;
+use Database\Seeders\ProductSeeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        //\App\Models\User::factory(10)->create();
+        \App\Models\Category::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        $this->call([ 
+        $this->call([
             UserSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
